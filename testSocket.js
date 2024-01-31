2fb87072f017c8cab282f8928dc1212799c1b5bc
DevHoyops <Ihoyos96@gmail.com>
2024-01-31 15:04:04 -0400

    misc

--- testSocket.js
+++ testSocket.js
@@ -1,7 +1,19 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const os = require('os');
 
+function getServerIPAddress() {
+    const interfaces = os.networkInterfaces();
+    for (let iface of Object.values(interfaces)) {
+        for (let alias of iface) {
+            if (alias.family === 'IPv4' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+    return 'localhost';
+}
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
@@ -10,5 +22,6 @@ io.on('connection', (socket) => {
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    const ip = getServerIPAddress();
+    console.log(`Server listening on http://${ip}:3000`);
 });
