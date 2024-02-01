838abd006ff6b97da3fff4154b08c97c5b831cd4
DevHoyops <Ihoyos96@gmail.com>
2024-02-01 16:54:28 -0400

    Working prototype

--- testSocket.js
+++ testSocket.js
@@ -1,7 +1,11 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 const os = require('os');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 function getServerIPAddress() {
     const interfaces = os.networkInterfaces();
@@ -14,14 +18,25 @@ function getServerIPAddress() {
     }
     return 'localhost';
 }
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+        ws.send(`Echo: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
 });
 
-http.listen(3000, () => {
+app.get('/', (req, res) => {
+    res.send('WebSocket server is running');
+});
+
+server.listen(3000, () => {
     const ip = getServerIPAddress();
     console.log(`Server listening on http://${ip}:3000`);
 });
